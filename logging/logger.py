1010a8c412dff8765e2542df6393e50c615423d3
Manish Surabhi <manishsurabhi23@gmail.com>
2026-01-08 17:38:19 -0500

    made some changes

--- logging/logger.py
+++ logging/logger.py
@@ -2,11 +2,14 @@ import logging
 import os
 from datetime import datetime
 LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-logs_path = os.path.join(os.getcwd(),"logs",LOG_FILE)
+logs_path = os.path.join(os.getcwd(),"logging",LOG_FILE)
 os.makedirs(logs_path,exist_ok=True)
 LOG_FILE_PATH = os.path.join(logs_path,LOG_FILE)
+logging.getLogger().handlers = []  # Clear existing handlers
 logging.basicConfig(
     filename=LOG_FILE_PATH,
     format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO,
 )
+#logger = logging.getLogger("my_agentics_app")
+#logger.info("test log message")
